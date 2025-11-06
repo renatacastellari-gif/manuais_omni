53ce0d597e695306aa0858798f0ba362d1e40544
renatacastellari-gif <renatacastellari@gmail.com>
2025-11-06 17:15:47 -0300

    Update Manuais.py

--- pages/Manuais.py
+++ pages/Manuais.py
@@ -1,19 +1,17 @@
 import streamlit as st
 
-st.set_page_config(page_title="Manual de Conciliação")
-
+# Configuração da página
 st.set_page_config(
     page_title="Manuais",
     page_icon="🟣",
 )
 
-
-
+# Logo centralizado
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
     st.image("teste.svg", width=300)
-    
-# Título principal
+
+# Título principal estilizado
 st.markdown("""
 <h2 style="
     color:#9B4DCC;
@@ -23,16 +21,21 @@ st.markdown("""
     border-bottom:2px solid #FFA500;
     padding-bottom:8px;
     margin-bottom:20px;">
-📘Manuais
+📘 Manuais
 </h2>
 """, unsafe_allow_html=True)
 
-("""**`Colaboradora: Patricia`** """)
+# Nome da colaboradora
+st.markdown("**`Colaboradora: Patricia`**")
 
+# Introdução
+st.markdown("""
+### 📥 Bem-vindo(a) à Página de Manuais dos Colaboradores
 
+Este espaço foi criado para reunir os principais manuais, orientações e procedimentos que apoiam o dia a dia dos colaboradores.  
+Aqui você encontrará informações úteis sobre processos internos, ferramentas utilizadas, boas práticas e responsabilidades de cada área.
 
-st.markdown("### 📥 Bem-vindo(a) à Página de Manuais dos Colaboradores
-Este espaço foi criado para reunir os principais manuais, orientações e procedimentos que apoiam o dia a dia dos colaboradores. Aqui você encontrará informações úteis sobre processos internos, ferramentas utilizadas, boas práticas e responsabilidades de cada área.
 Nosso objetivo é facilitar o acesso ao conhecimento, promover autonomia e garantir que todos tenham os recursos necessários para desempenhar suas atividades com excelência.
-Explore os conteúdos disponíveis e, em caso de dúvidas ou sugestões, entre em contato")
 
+Explore os conteúdos disponíveis e, em caso de dúvidas ou sugestões, entre em contato com o time responsável.
+""")
